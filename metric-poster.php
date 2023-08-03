c63d8b2017e8ec50601182e50a4371482ec9c80f
Omar Serrano <omar.serrano@automattic.com>
2023-08-03 10:39:32 -0600

    removing error

--- metric-poster.php
+++ metric-poster.php
@@ -10,11 +10,6 @@ declare(strict_types=1);
 
 namespace MetricPoster;
 
-// error logging. 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 // cant access this directly.
 if (!defined('ABSPATH')) {
 	exit;
