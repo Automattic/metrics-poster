b05635afa241d7dd87c3bb62530e256d1dcda824
Omar Serrano <omar.serrano@automattic.com>
2024-01-08 16:52:44 -0700

    adding inp support

--- metric-poster.php
+++ metric-poster.php
@@ -4,7 +4,7 @@
  * Plugin Name: Metric Poster
  * Plugin URI:
  * Description: A plugin to generate a post from a template and post it to a P2 site.
- * Version: 1.0.7
+ * Version: 1.0.8
  */
 
 declare(strict_types=1);
