2f46ce1ab7c20a052caa6075ce549ad6763af77c
Omar Serrano <omar.serrano@automattic.com>
2024-02-12 16:58:17 -0700

    updating m plugin version

--- metric-poster.php
+++ metric-poster.php
@@ -4,7 +4,7 @@
  * Plugin Name: Metric Poster
  * Plugin URI: https://github.com/Automattic/metrics-poster
  * Description: A plugin to generate a post from a template and post it to a P2 site.
- * Version: 1.0.18
+ * Version: 1.0.19
  */
 
 declare(strict_types=1);
